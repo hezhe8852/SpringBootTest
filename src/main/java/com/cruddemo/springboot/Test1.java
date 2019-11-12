1d6c5c4d9ec08c05bd43bb1e424aa42317a9e9bf
hezhe8852 <471577873@qq.com>
2019-11-12 21:17:25 +0800

    firist commit

--- src/main/java/com/cruddemo/springboot/Test1.java
+++ src/main/java/com/cruddemo/springboot/Test1.java
@@ -1,21 +1,17 @@
 package com.cruddemo.springboot;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Test1 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<String>();
-        get(list);
+        for (String string : list) {
+            System.out.println(string);
 
-
-    }
-
-    private static void get(List<String> list) {
-        for (String ACV : list) {
-            System.out.println("AAA");
-            if (ACV != null) {
-
-            }
         }
+
+
     }
+
 }
